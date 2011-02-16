4e23f29fe75c576eb5ea4592ca544b21d354306a
Noctivity Inc <jlippiner@noctivity.com>
2011-02-16 12:26:20 -0500

    removed get friends from home js

--- public/javascripts/controllers/home.js
+++ public/javascripts/controllers/home.js
@@ -1,31 +1,4 @@
 var intId;
-var allFriends;
-var friendPosition = 1;
-
-function getFriends() {
-  var query = FB.Data.query("SELECT name, pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me()) ORDER BY rand() LIMIT 10");
-   query.wait(function(rows) {
-      allFriends = rows;
-      row = allFriends[0];
-      $(document.createElement("img"))
-          .attr({ src: row.pic_square, title: row.name })
-          .addClass("friend")
-          .appendTo('#friends')
-      intId = window.setInterval('showFriends()',3500)
-   });
-}
-
-function showFriends () {
-  for(x = friendPosition;x<friendPosition+3;x++) {
-    row = allFriends[x];
-    $(document.createElement("img"))
-        .attr({ src: row.pic_square, title: row.name })
-        .addClass("friend")
-        .appendTo('#friends')
-  }
-  friendPosition = friendPosition + 3;
-  if(friendPosition>=allFriends.length) window.clearInterval(intId);
-}
 
 function showMe() {
       FB.api('/me', function(r) {
@@ -37,7 +10,6 @@ function showMe() {
               window.clearInterval(intId);
               FB.logout(function(resp){
                 $('#not_user').hide();
-                $('.friend').hide();
               });
               return false;
             })
@@ -48,7 +20,6 @@ $(document).ready(function() {
   FB.getLoginStatus(function(response) {
     if (response.session) {
       showMe()
-      getFriends();
       $('#header').load('/header',{uid: response.session.uid})
     } else {
     }
